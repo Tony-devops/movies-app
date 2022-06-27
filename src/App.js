b8d5be6da4c4e8543258d40d035d1ac9e79935d9
OlgitH <berman.oj@gmail.com>
2022-06-27 13:34:39 +0100

    ~

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/css/App.css';
+import { useEffect, useState } from 'react';
+import ClipLoader from "react-spinners/ClipLoader";
 
 function App() {
+
+  const [movies, setMovies] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    
+    fetch('https://showtimes.everyday.in.th/api/v2/movie/')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setMovies(data.results);
+        setLoading(false);
+      });
+  },[])  
+
+  useEffect(() => {
+    console.log('movies: ', movies )
+  },[movies])  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movies</h1>
+      {
+        loading ? 
+          <div><ClipLoader color={'red'} size={20} /><br /> Loading movies ... </div>
+        :
+          <div style={{display: 'flex'}}>
+            {
+              movies.length > 0 &&
+              movies.map((movie, i) => {
+                return (
+                  <div key={i} style={{border: '1px solid black'}}>
+                    {movie.title}
+                    <img src={movie.images[0].url} width="100" />
+                  </div> 
+                )
+              })
+            }
+            </div>
+      }
     </div>
   );
+
 }
 
 export default App;
